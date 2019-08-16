23fe0b360101471e99cb46f7710041aafbdc43fd
Shakila Sasikaran <srajah.shakila@gmail.com>
2019-08-16 12:48:34 +0530

    Add validation methods

--- utils/src/main/java/org/wso2/ei/module/strings/StringValidations.java
+++ utils/src/main/java/org/wso2/ei/module/strings/StringValidations.java
@@ -1,12 +1,40 @@
 package org.wso2.ei.module.strings;
 
+import com.google.common.net.InetAddresses;
 import org.apache.commons.validator.routines.EmailValidator;
+import org.apache.commons.validator.routines.UrlValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class StringValidations {
+    private static final Logger log = LoggerFactory.getLogger(StringValidations.class);
 
     private StringValidations() {}
 
-    public static boolean validateEmail(String input) {
-        return EmailValidator.getInstance().isValid(input);
+    public static boolean validateEmail(String email) {
+        return EmailValidator.getInstance().isValid(email);
+    }
+
+    public static boolean validateIP(String ip) {
+        return InetAddresses.isInetAddress(ip);
+    }
+
+    public static boolean containsIP(String ip, String... ipList) {
+        for (int i = 0; i < ipList.length; i++) {
+            try {
+                return InetAddress.getByName(ip).equals(InetAddress.getByName(ipList[i]));
+            } catch (UnknownHostException e) {
+                log.info("Validation failed : " + e.getMessage());
+            }
+        }
+        return false;
+    }
+
+    public static boolean validateURL(String url) {
+        UrlValidator urlValidator = new UrlValidator();
+        return urlValidator.isValid(url);
     }
 }
